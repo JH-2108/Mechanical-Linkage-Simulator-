378d5ac3ca60cff5c020f7708c36fea3cb59f7d0
Jie Hao <jiehao.chin@gmail.com>
2025-11-09 09:48:12 +0800

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from linkage import Linkage
+from visualize import LinkageVisualizer
+
+example_file = "examples/four_bar_example.json"
+with open(example_file, "r") as f:
+    mech_data = json.load(f)
+
+linkage = Linkage(mech_data["links"], mech_data["joints"])
+
+visualizer = LinkageVisualizer(linkage, mech_data["mechanism_name"])
+visualizer.animate()
 
 example_file = "examples/four_bar_example.json" 
 with open(example_file, "r") as f:
